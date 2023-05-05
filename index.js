3892cc0bf31374b5ddbc443d9426af8231da20d7
Radim Vrška <87022843+radim567@users.noreply.github.com>
2023-05-05 14:23:27 +0200

    testing api

--- index.js
+++ index.js
@@ -2,13 +2,46 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.get('/', (req, res) =>
+const members = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    status: 'active',
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    email: 'janesmith@example.com',
+    status: 'inactive',
+  },
+  {
+    id: 3,
+    name: 'Bob Johnson',
+    email: 'bobjohnson@example.com',
+    status: 'active',
+  },
+  {
+    id: 4,
+    name: 'Alice Brown',
+    email: 'alicebrown@example.com',
+    status: 'inactive',
+  },
+];
+
+/* app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 );
 
 app.get('/about', (req, res) =>
   res.sendFile(path.join(__dirname, 'public', 'about.html'))
-);
+); */
+
+// Get all members
+app.get('/api/members', (req, res) => res.json(members));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 
