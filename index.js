2bcf256f434e5a0ff7395f99ec9aaafaf0daf5f4
Radim Vrška <87022843+radim567@users.noreply.github.com>
2023-05-08 10:06:58 +0200

    PROJECT DONE

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
-
+// const { engine } = require('express-handlebars');
+const exphbs = require('express-handlebars');
+const engine = exphbs.engine;
 const logger = require('./middleware/logger');
 const app = express();
+const members = require('./Members');
 
 /* app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
@@ -12,9 +15,18 @@ app.get('/about', (req, res) =>
   res.sendFile(path.join(__dirname, 'public', 'about.html'))
 ); */
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Homepage route
+app.get('/', (req, res) =>
+  res.render('index', { title: 'Member App', members })
+);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
